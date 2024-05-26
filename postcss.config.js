42d0557a0f5fc8cb2e8ceacd714da17ca9e005ad
Soham <rockstarsam1234@gmail.com>
2024-05-26 20:23:39 +0530

    fixed css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [require('tailwindcss'), require('autoprefixer'), require('postcss-100vh-fix')],
+};
