71a311c0f816a37ba08527462642af483a75e7ba
Debjit Majumder <Debjit.Majumder@connectwise.com>
2024-06-01 17:12:53 +0530

    force dynamic

--- src/app/api/integrations/route.ts
+++ src/app/api/integrations/route.ts
@@ -1,9 +1,11 @@
-import { headers } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+// import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 import IntegrationRepository from '../db/repositories/integration.repository';
-import UserRepository from '../db/repositories/user.repository';
-import { getUidFromIdToken } from '../firebase/utils';
+// import UserRepository from '../db/repositories/user.repository';
+// import { getUidFromIdToken } from '../firebase/utils';
+
+export const dynamic = 'force-dynamic' 
 
 export async function GET() {
   const integrationRepository = new IntegrationRepository();
