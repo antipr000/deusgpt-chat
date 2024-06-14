49dc867756d28cd94aaf5a246e6cbd7caac6bb8d
Debjit Majumder <Debjit.Majumder@connectwise.com>
2024-06-14 23:08:27 +0530

    refactor translate page

--- src/app/translate/layout.tsx
+++ src/app/translate/layout.tsx
@@ -5,33 +5,18 @@ import { Flexbox } from 'react-layout-kit';
 
 import '@/styles/global.css';
 
-// import ConfigDrawerLayout from './ConfigDrawerLayout';
-import ConfigPanel from './components/ConfigPanel';
 import { GlobalProvider } from './components/GlobalStore';
 import { GlobalToaster } from './components/GlobalToaster';
-import Header from './components/Header';
-import NavBar from './components/NavBar';
 import { ReactQueryProvider } from './components/ReactQueryProvider';
-import ConfigPage from './pages/Config';
 
 const TranslateLayout = ({ children }: { children: ReactNode }) => {
   return (
     <ReactQueryProvider>
       <GlobalProvider>
         <GlobalToaster />
-        {/* <ConfigDrawerLayout>
-          <Header />
-          {children}
-          <NavBar />
-        </ConfigDrawerLayout> */}
         <Flexbox direction="horizontal" style={{ width: '100% ' }}>
-          <ConfigPanel>
-            <ConfigPage />
-          </ConfigPanel>
           <Flexbox style={{ flex: '1 0 auto' }}>
-            <Header />
             {children}
-            <NavBar />
           </Flexbox>
         </Flexbox>
       </GlobalProvider>
--- src/app/translate/pages/Translator.tsx
+++ src/app/translate/pages/Translator.tsx
@@ -1,35 +1,26 @@
 'use client';
 
-import { TextArea } from '@lobehub/ui';
-import { Select } from 'antd';
-import { Button as AntButton } from 'antd';
-import clsx from 'clsx';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { Button } from 'react-daisyui';
-import toast from 'react-hot-toast';
+import { useCallback, useEffect, useState } from 'react';
+import { App, Button } from 'antd';
 import { useTranslation } from 'react-i18next';
-import { CgArrowsExchange } from 'react-icons/cg';
-import { MdClose, MdContentCopy } from 'react-icons/md';
+import { MdOutlineSwapHoriz } from 'react-icons/md';
+import { TbCopy } from 'react-icons/tb';
 import 'regenerator-runtime/runtime';
 
 import { useGlobalStore } from '../components/GlobalStore';
-// import { SpeechRecognitionButton } from '../components/SpeechRecognitionButton';
-import { TTSButton } from '../components/TTSButton';
 import { LANGUAGES, Language } from '../constants';
 import { getTranslatePrompt } from '../utils/prompt';
 
 function TranslatorPage() {
   const { t, i18n } = useTranslation('translate');
-  const translateTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const [isClient, setIsClient] = useState(false);
+  const { message } = App.useApp();
 
   const {
     configValues: { currentModel, temperatureParam },
     translator: {
       lastTranslateData,
       setLastTranslateData,
-      translateText,
-      setTranslateText,
       translatedText,
       mutateTranslateText,
       isTranslating,
@@ -43,21 +34,21 @@ function TranslatorPage() {
     if (!isTranslateError) {
       return;
     }
-    toast.error(t('Something went wrong, please try again later.'));
+    message.error(t('Something went wrong, please try again later.'));
   }, [isTranslateError, t]);
 
   const onCopyBtnClick = useCallback(() => {
     if (!translatedText) {
-      toast.error(t('Nothing to copy!'));
+      message.error(t('Nothing to copy!'));
       return;
     }
     navigator.clipboard
       .writeText(translatedText)
       .then(() => {
-        toast.success(t('Copied!'));
+        message.success(t('Copied!'));
       })
       .catch(() => {
-        toast.error(t('Failed to copy!'));
+        message.error(t('Failed to copy!'));
       });
   }, [t, translatedText]);
 
@@ -75,29 +66,15 @@ function TranslatorPage() {
     [setLastTranslateData],
   );
 
-  const onChangeTranscript = useCallback(
-    (newTranscript: string) => {
-      if (!translateTextAreaRef.current || !newTranscript) {
-        return;
-      }
-      translateTextAreaRef.current.value = newTranscript;
-      translateTextAreaRef.current.defaultValue = newTranscript;
-      setTranslateText(newTranscript);
-    },
-    [setTranslateText],
-  );
-
   const handleTranslate = useCallback(
-    (event: React.FormEvent<HTMLFormElement>) => {
+    (event: any) => {
       event.preventDefault();
       const { fromLang, toLang } = lastTranslateData;
       console.log('Here 2', toTranslateText, fromLang, toLang);
       if (!toTranslateText || !fromLang || !toLang) {
-        toast.error('From and to languages are mandatory!');
+        message.error('From and to languages are mandatory!');
         return;
       }
-
-      setToTranslateText(toTranslateText);
 
       let prompt: string;
 
@@ -112,12 +89,6 @@ function TranslatorPage() {
         prompt = getTranslatePrompt(fromLang as Language, toLang as Language);
       }
 
-      setLastTranslateData((prev) => ({
-        ...prev,
-        fromLang: fromLang as Language,
-        toLang: toLang as Language,
-      }));
-
       mutateTranslateText({
         engine: currentModel,
         prompt,
@@ -129,35 +100,29 @@ function TranslatorPage() {
       currentModel,
       i18n.language,
       mutateTranslateText,
-      setLastTranslateData,
-      setTranslateText,
       toTranslateText,
-      t,
-      temperatureParam,
+      lastTranslateData
     ],
   );
-
-  const onClearBtnClick = useCallback(() => {
-    setTranslateText('');
-  }, []);
-
-  // ↑ Hooks before, keep hooks order
 
   if (!isClient) return null;
 
   return (
-    <form method="post" onSubmit={handleTranslate}>
-      <div className="container max-w-screen-2xl xl:mx-auto md:grid md:grid-cols-2 md:gap-4 bg-base-100">
-        <div className="w-full md:min-h-[calc(100vh_-_112px)] max-w-full p-4 m-0 shadow-md top-16 bg-base-100">
-          <div className="flex items-center flex-row mb-4">
-            {/* <select
-              className="w-5/12 select white-text"
-              name="fromLang"
+    <div className="min-h-screen flex items-start justify-center bg-[#f5f5f5]">
+      <div className="rounded-lg w-full">
+        <h1 className="text-2xl font-semibold mb-4 text-center">Translator</h1>
+
+        <div className="flex space-x-4 mb-4">
+          <div className="flex-1">
+            <label className="block text-sm font-medium text-gray-700 mb-2" htmlFor="sourceLang">
+              Source Language
+            </label>
+            <select
+              className="block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm p-2 outline-none"
+              id="sourceLang"
               onChange={(e) =>
                 setLastTranslateData((prev) => ({ ...prev, fromLang: e.target.value }))
               }
-              required
-              title="From Language"
               value={lastTranslateData.fromLang}
             >
               {Object.keys(LANGUAGES).map((lang) => (
@@ -165,155 +130,80 @@ function TranslatorPage() {
                   {LANGUAGES[lang as Language]}
                 </option>
               ))}
-            </select> */}
-            <Select
-              className="w-5/12 h-[55px]"
-              onChange={(value) => setLastTranslateData((prev) => ({ ...prev, fromLang: value }))}
-              options={Object.entries(LANGUAGES).map(([value, label]) => ({
-                label,
-                value,
-              }))}
-              title="Language From"
-              value={lastTranslateData.fromLang}
-            />
-
-            <div className="flex justify-center w-2/12">
-              <Button
-                className="white-text"
-                color="ghost"
-                onClick={onExchangeLanguageBtnClick}
-                shape="circle"
-                title="Exchange"
-                type="button"
-              >
-                <CgArrowsExchange size={20} />
-              </Button>
-            </div>
-
-            <Select
-              className="w-5/12 h-[55px]"
-              onChange={(value) => setLastTranslateData((prev) => ({ ...prev, toLang: value }))}
-              options={Object.entries(LANGUAGES).map(([value, label]) => ({
-                label,
-                value,
-              }))}
-              title="To language"
-              value={lastTranslateData.toLang}
-            />
+            </select>
           </div>
-
-          <div className="form-control">
-            <div className="relative">
-              <TextArea
-                className="w-full mt-4 mb-2 whitespace-pre-line break-words resize-none 
-                rounded-2xl textarea textarea-md textarea-primary md:min-h-[120px] pb-10 white-text"
-                disabled={isTranslating}
-                name="translateText"
-                onChange={(e) => setToTranslateText(e.target.value)}
-                placeholder={t('Please enter the text you want to translate here.')}
-                ref={translateTextAreaRef}
-                required
-                value={toTranslateText}
-              />
-              <div className="absolute flex flex-row justify-between left-0 bottom-5 w-full px-2">
-                <div className="flex flex-row justify-start gap-2">
-                  {/* <SpeechRecognitionButton
-                    language={
-                      lastTranslateData.fromLang === 'auto'
-                        ? i18n.language
-                        : lastTranslateData.fromLang
-                    }
-                    onChangeTranscript={onChangeTranscript}
-                    disabled={isTranslating}
-                  /> */}
-                  {!!translateText && (
-                    <TTSButton
-                      className="white-text"
-                      language={
-                        lastTranslateData.fromLang === 'auto' ? 'en-US' : lastTranslateData.fromLang
-                      }
-                      text={translateText}
-                    />
-                  )}
-                </div>
-                {!!translateText && (
-                  <Button
-                    className="white-text"
-                    color="ghost"
-                    onClick={onClearBtnClick}
-                    shape="circle"
-                    size="sm"
-                    title="Clear the input"
-                    type="button"
-                  >
-                    <MdClose size="16" />
-                  </Button>
-                )}
-              </div>
-            </div>
-            <AntButton
-              className="md:hidden"
-              type="primary"
-              disabled={isTranslating}
-              loading={isTranslating}
-              htmlType="submit"
+          <Button className="mt-6" onClick={onExchangeLanguageBtnClick}>
+            <MdOutlineSwapHoriz className="w-5 h-5" />
+          </Button>
+          <div className="flex-1">
+            <label className="block text-sm font-medium text-gray-700 mb-2" htmlFor="targetLang">
+              Target Language
+            </label>
+            <select
+              className="block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm p-2 outline-none"
+              id="targetLang"
+              onChange={(e) =>
+                setLastTranslateData((prev) => ({ ...prev, toLang: e.target.value }))
+              }
+              value={lastTranslateData.toLang}
             >
-              {isTranslating ? t('Translating...') : t('Translate')}
-            </AntButton>
+              {Object.keys(LANGUAGES).map((lang) => (
+                <option key={lang} value={lang}>
+                  {LANGUAGES[lang as Language]}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
-        <div className="p-4 pb-14 m-0 form-control">
-          <AntButton
-            type="primary"
-            disabled={isTranslating}
-            loading={isTranslating}
-            style={{
-              width: '150px',
-            }}
-            htmlType="submit"
-          >
-            {isTranslating ? t('Translating...') : t('Translate')}
-          </AntButton>
-          <div className="relative">
-            <TextArea
-              className={clsx(
-                'w-full mb-2 mt-5 white-text whitespace-pre-line break-words resize-none rounded-2xl textarea textarea-md textarea-ghost md:min-h-[120px]',
-                !!translatedText && !isTranslating && 'pb-10',
-              )}
-              name="translatedText"
-              placeholder={
-                isTranslating ? t('Please wait...') : t('Translated text will appear here.')
-              }
-              readOnly
-              required
-              value={translatedText || ''}
+
+        <div className="flex space-x-4 mb-4">
+          <div className="flex-1">
+            <label className="block text-sm font-medium text-gray-700 mb-2" htmlFor="sourceText">
+              Source Text
+            </label>
+            <textarea
+              className="block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm p-2 outline-none"
+              cols={20}
+              id="sourceText"
+              onChange={(e) => setToTranslateText(e.target.value)}
+              placeholder="Please enter the text you want to translate here."
+              rows={7}
+              value={toTranslateText}
             />
-            <div className="absolute flex flex-row justify-between left-0 bottom-5 w-full px-2">
-              {!!translatedText && (
-                <TTSButton
-                  language={
-                    lastTranslateData.toLang === 'auto' ? 'en-US' : lastTranslateData.toLang
-                  }
-                  text={translatedText || ''}
-                />
-              )}
-              {!!translatedText && !isTranslating && (
-                <Button
-                  color="ghost"
-                  onClick={onCopyBtnClick}
-                  shape="circle"
-                  size="sm"
-                  title={t('Copy translated text')}
-                  type="button"
-                >
-                  <MdContentCopy size="16" />
-                </Button>
-              )}
-            </div>
           </div>
+          <div className="flex-1 relative">
+            <label
+              className="block text-sm font-medium text-gray-700 mb-2"
+              htmlFor="translatedText"
+            >
+              Translated Text
+            </label>
+            <textarea
+              className="block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm p-2 outline-none bg-gray-100"
+              cols={10}
+              id="translatedText"
+              placeholder="Translated text will appear here."
+              readOnly
+              rows={7}
+              value={translatedText}
+            />
+            <Button
+              className="absolute bottom-4 right-3"
+              disabled={!translatedText}
+              onClick={onCopyBtnClick}
+            >
+              <TbCopy className="w-5 h-5" />
+            </Button>
+          </div>
+        </div>
+
+        <div className="text-end">
+          <Button className='bg-black text-white' disabled={isTranslating} onClick={handleTranslate}>
+            Translate
+          </Button>
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
